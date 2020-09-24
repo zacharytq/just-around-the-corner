abdabb217eed0d355dae47bc88c1a7da409e5839
zacharytq <zacharytq@gmail.com>
2020-09-24 11:06:06 -0400

    signup and login routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
 
     get '/signup' do
-        erb :'/users/signup'
+        if logged_in?
+            redirect to "/friends"
+        else
+            erb :"/users/signup"
+        end
+    end
+
+    get '/login' do
+        if logged_in?
+            redirect to '/friends'
+        else
+            erb :'/users/login'
+        end
     end
 end
