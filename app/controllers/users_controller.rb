e558b76b0b814bd5778c475475002ca6078ae8ac
zacharytq <zacharytq@gmail.com>
2020-09-24 11:11:02 -0400

    login and signup post routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,31 @@ class UsersController < ApplicationController
         end
     end
 
+    post '/signup' do
+        @user = User.new(params)
+        if @user.save
+            session[:id] = @user.id
+            redirect to "/friends"
+        else
+            redirect to '/signup'
+        end
+    end
+
     get '/login' do
         if logged_in?
             redirect to '/friends'
         else
             erb :'/users/login'
+        end
+    end
+
+    post '/login' do
+        @user = User.find_by(:username => params[:name])
+        if @user && @user.authenticate(params[:password])
+            session[:id] = @user.id
+            redirect to "/friends"
+        else
+            redirect to '/login'
         end
     end
 end
