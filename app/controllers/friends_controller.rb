4a8cd4eaa012a75d90e8176db951a7538f7484bb
zacharytq <zacharytq@gmail.com>
2020-09-24 11:17:49 -0400

    wrote friends get route

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,2 +1,11 @@
 class FriendsController < ApplicationController
+    
+    get '/friends' do
+        if logged_in?
+            @user = current_user
+            erb :'/friends/show'
+        else
+            redirect to '/'
+        end
+    end
 end
