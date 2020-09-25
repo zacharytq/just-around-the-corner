c8e49d704fefc3cfffc6642315d9e1304e005771
zacharytq <zacharytq@gmail.com>
2020-09-25 15:17:34 -0400

    show route for 1 friend written

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -3,6 +3,15 @@ class FriendsController < ApplicationController
     get '/friends' do
         if logged_in?
             @user = current_user
+            erb :'/friends/all'
+        else
+            redirect to '/'
+        end
+    end
+
+    get '/friends/:id' do
+        @friend = Friend.find(params[:id])
+        if logged_in? && current_user = @friend.user
             erb :'/friends/show'
         else
             redirect to '/'
