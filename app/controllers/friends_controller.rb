23f2d84dd6efb8d128377b271b7b429b712faca3
zacharytq <zacharytq@gmail.com>
2020-09-26 11:43:04 -0400

    can add a new friend

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -17,4 +17,27 @@ class FriendsController < ApplicationController
             redirect to '/'
         end
     end
+
+    patch "/friends/:id/save" do
+        if logged_in?
+            @friend = Friend.find(params[:id])
+            if @friend.user == current_user
+                params[:friend][:gift_ids].each do |id|
+                   gift = Gift.find(id)
+                   gift.purchased = true
+                   gift.save
+                end
+                redirect to "/friends/#{@friend.id}"
+            end
+        else
+            redirect to "/"
+        end
+    end
+    
+    post "/friends/new" do
+        @user  = current_user
+        @friend = Friend.new(name: params[:name])
+        @user.friends << @friend
+        redirect to "/friends/#{@friend.id}"
+    end
 end
