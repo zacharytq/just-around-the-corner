4b9ae14a2d0a0665599d454f8ce565ca38844c2c
zacharytq <zacharytq@gmail.com>
2020-09-26 12:04:17 -0400

    make new gift route and view made

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -13,4 +13,27 @@ class GiftsController < ApplicationController
             redirect to "/"
         end
     end
+
+    get "/gifts/:id/new" do
+        if logged_in?
+            @friend = Friend.find(params[:id])
+            erb :'/gifts/new'
+        else
+            redirect to '/'
+        end
+    end
+
+    post "/gifts/new" do
+        @friend = Friend.find(params[:friend])
+        @gift = Gift.new(name: params[:name])
+        @gift.price = params[:price].to_i
+        @gift.url = params[:url]
+        @gift.friend = @friend
+        if @gift.save
+            redirect to "/friends/#{@friend.id}"
+        else
+            redirect to "/gifts/#{@friend.id}/new"
+        end
+    end
+
 end
