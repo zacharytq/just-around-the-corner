c8ffdf2be10dc33d1def83bcbc9718871a88baf8
zacharytq <zacharytq@gmail.com>
2020-09-27 13:52:39 -0400

    used bootstrap to style gifts page

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,14 +1,12 @@
 class GiftsController < ApplicationController
 
-    get "/gifts/:id/delete" do
-        if logged_in?
-            @gift = Gift.find(params[:id])
-            @friend = @gift.friend
-            @user = @gift.friend.user
-            if current_user == @user
-                @gift.destroy
-                redirect to "/friends/#{@friend.id}"
-            end
+
+    delete "/gifts/:id" do
+        @gift = Gift.find(params[:id])
+        @friend = @gift.friend
+        if logged_in? && current_user == @gift.friend.user
+            @gift.destroy
+            redirect to "/friends/#{@friend.id}"
         else
             redirect to "/"
         end
